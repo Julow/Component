c1142dea6229a96138a8198d8a35bda3c4a2db1e
juloo <juloo.dsi@gmail.com>
2017-08-20 20:45:53 +0200

    Fix incorrect destroy and useless redraw

--- Component.ml
+++ Component.ml
@@ -6,7 +6,7 @@
 (*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2017/03/15 17:35:56 by jaguillo          #+#    #+#             *)
-(*   Updated: 2017/07/05 22:19:35 by jaguillo         ###   ########.fr       *)
+(*   Updated: 2017/08/20 20:43:04 by juloo            ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -15,19 +15,32 @@ type ('a, 'e) tmpl = 'a -> ('e -> unit) -> (
 	) * (unit -> unit)
 
 let run (t, tasks) view update =
-	let stream, push = Lwt_stream.create () in
-	let push e = push (Some e) in
-	let mount, deinit = view t push in
-	let redraw, unmount = mount () in
-	let rec loop t tasks =
-		List.iter (fun t -> Lwt.on_success t push) tasks;
-		redraw t;
-		let%lwt event = Lwt_stream.next stream in
-		match update t event with
-		| `Loop (t, tasks)	-> loop t tasks
-		| `Done r			-> Lwt.return r
+	let pull, push =
+		let stream, push = Lwt_stream.create () in
+		let pull () = Lwt_stream.next stream in
+		let push e = push (Some e) in
+		pull, push
 	in
-	let r = loop t tasks in
-	unmount ();
-	deinit ();
-	r
+	let redraw, destroy =
+		let mount, deinit = view t push in
+		let redraw, unmount = mount () in
+		let destroy () =
+			unmount ();
+			deinit ()
+		in
+		redraw, destroy
+	in
+	let pull_tasks = List.iter (fun t -> Lwt.on_success t push) in
+	let rec loop t =
+		let%lwt event = pull () in
+		match update t event with
+		| `Loop (t, tasks)	->
+			pull_tasks tasks;
+			redraw t;
+			loop t
+		| `Done r			->
+			destroy ();
+			Lwt.return r
+	in
+	pull_tasks tasks;
+	loop t
