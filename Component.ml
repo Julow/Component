f30eb1497fcde95654a537bf621cc7c907e97400
juloo <juloo.dsi@gmail.com>
2017-10-06 00:30:43 +0200

    Change the type of controlers

--- Component.ml
+++ Component.ml
@@ -6,11 +6,15 @@
 (*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2017/03/15 17:35:56 by jaguillo          #+#    #+#             *)
-(*   Updated: 2017/08/20 21:05:09 by juloo            ###   ########.fr       *)
+(*   Updated: 2017/10/06 00:26:55 by juloo            ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 type ('a, 'e) root = 'a -> ('e -> unit) -> ('a -> unit) * (unit -> unit)
+
+type ('a, 'e) task = Task of (unit -> ('a, 'e) controler Lwt.t)
+and ('a, 'e) loop = 'e * ('a, 'e) task list
+and ('a, 'e) controler = 'a -> ('a, 'e) loop
 
 let run (t, tasks) root update =
 	let pull, push =
@@ -20,17 +24,17 @@ let run (t, tasks) root update =
 		pull, push
 	in
 	let redraw, destroy = root t push in
-	let pull_tasks = List.iter (fun t -> Lwt.on_success t push) in
+	let pull_tasks = List.iter (fun (Task t) -> Lwt.on_success (t ()) push) in
 	let rec loop t =
 		let%lwt event = pull () in
-		match update t event with
-		| `Loop (t, tasks)	->
-			pull_tasks tasks;
-			redraw t;
-			loop t
-		| `Done r			->
-			destroy ();
-			Lwt.return r
+		let t, tasks = update t event in
+		pull_tasks tasks;
+		redraw t;
+		loop t
 	in
-	pull_tasks tasks;
-	loop t
+	try
+		pull_tasks tasks;
+		loop t
+	with e ->
+		destroy ();
+		raise e
